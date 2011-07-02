3cbee0e9239c258ab70cd6f90829c5bbe190e0fb
Christian Horton <christian@tweakuniverse.com>
2011-07-02 15:50:36 -0700

    Removed Cmd folder

--- cmd/Compile.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-Title ownigepkz Compiler
-cd ..
-"C:\Program Files\Java\jdk1.6.0_16\bin\javac.exe" -cp . -d ./Bin/ ./Bulby/./io/*.java
-pause
--- cmd/runserver2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Title NineScape
-cd ../bin
-color 40
-"C:\Program Files\Java\jre6\bin\java.exe" Bulby/Server 43594
-PAUSE
